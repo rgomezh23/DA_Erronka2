3ac94f01b718615abde037235db105d489f342a6
Oier592 <ogarciam23@fpsanturtzilh.eus>
2025-02-06 13:36:23 +0100

    Zerbitzuak_funcional

--- src/main/java/eus/fpsanturtzilh/controllers/ZerbitzuakController.java
+++ src/main/java/eus/fpsanturtzilh/controllers/ZerbitzuakController.java
@@ -5,12 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import eus.fpsanturtzilh.models.Zerbitzuak;
 import eus.fpsanturtzilh.services.ZerbitzuakService;
@@ -23,19 +18,76 @@ public class ZerbitzuakController {
 	private ZerbitzuakService zerbitzuService;
 
 	@CrossOrigin(origins = "http://localhost:8100")
-	@GetMapping("/zerbitzuGuztiak")
-	public List<Zerbitzuak> getZerbitzuak() {
-		return zerbitzuService.getAllZerbitzuak();
+	@GetMapping("/aktiboak")
+	public ResponseEntity<List<Zerbitzuak>> getZerbitzuakEzabatzeDataNull() {
+		try {
+			List<Zerbitzuak> zerbitzuak = zerbitzuService.getZerbitzuakWithEzabatzeDataNull();
+			return ResponseEntity.ok(zerbitzuak);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 
 	@CrossOrigin(origins = "http://localhost:8100")
-	@PutMapping(value = "/update", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<Zerbitzuak> updateProduktu(@RequestBody Zerbitzuak zerbitzua) {
+	@GetMapping("/ez-aktiboak")
+	public ResponseEntity<List<Zerbitzuak>> getZerbitzuakEzabatzeDataNotNull() {
 		try {
-			Zerbitzuak updatedProduct = zerbitzuService.updateZerbitzuak(zerbitzua);
-			return ResponseEntity.ok(updatedProduct);
+			List<Zerbitzuak> zerbitzuak = zerbitzuService.getZerbitzuakWithEzabatzeDataNotNull();
+			return ResponseEntity.ok(zerbitzuak);
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	@CrossOrigin(origins = "http://localhost:8100")
+	@PostMapping(value = "/update", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Zerbitzuak> updateZerbitzuak(@RequestBody Zerbitzuak zerbitzua) {
+		try {
+			Zerbitzuak updatedZerbitzu = zerbitzuService.updateZerbitzuak(zerbitzua);
+			return ResponseEntity.ok(updatedZerbitzu);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	@CrossOrigin(origins = "http://localhost:8100")
+	@PutMapping(value = "/insert", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Zerbitzuak> insertZerbitzuak(@RequestBody Zerbitzuak zerbitzua) {
+		try {
+			Zerbitzuak newZerbitzu = zerbitzuService.insertZerbitzuak(zerbitzua);
+			return ResponseEntity.ok(newZerbitzu);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	@CrossOrigin(origins = "http://localhost:8100")
+	@DeleteMapping("/softDelete/{id}")
+	public ResponseEntity<String> softDeleteZerbitzuak(@PathVariable int id) {
+		try {
+			boolean result = zerbitzuService.softDeleteZerbitzuak(id);
+			if (result) {
+				return ResponseEntity.ok("Zerbitzua ezabatuta");
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Zerbitzua ez da aurkitu");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore bat egon da.");
+		}
+	}
+
+	@CrossOrigin(origins = "http://localhost:8100")
+	@DeleteMapping("/hardDelete/{id}")
+	public ResponseEntity<String> hardDeleteZerbitzuak(@PathVariable int id) {
+		try {
+			boolean result = zerbitzuService.hardDeleteZerbitzuak(id);
+			if (result) {
+				return ResponseEntity.ok("Zerbitzua guztiz ezabatuta");
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Zerbitzua ez da aurkitu");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore bat egon da.");
 		}
 	}
 }
